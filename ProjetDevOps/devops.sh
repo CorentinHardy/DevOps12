437e2bc0bf723dd8a15b39b8b8860e10fa27a894
GregoryROBIN <robin.gregory.gr@gmail.com>
2016-02-25 19:35:06 +0100

    "devops.sh"

---
+++ ProjetDevOps/devops.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Démarrage du test par mutation"
+PATH_DEVOPS=$(pwd)
+PATH_SOURCE=$PATH_DEVOPS/DevopsEntree
+PATH_MUTANT=$PATH_DEVOPS/DevopsMutation/src/main/java/DevopsTest/mutators/
+
+ID="DevopsTest.mutators."
+
+
+cd "$PATH_MUTANT"
+ls *.java > "$PATH_DEVOPS"/processor.txt
+cd  "$PATH_DEVOPS"
+while read ligne  
+do  
+   PROCESSOR=$(basename $ligne  '.java')
+echo -e "\n\n########################################################################################"
+echo "########################################################################################"
+echo " APPLICATION DU MUTANT $PROCESSOR"
+echo "########################################################################################"
+echo -e "########################################################################################\n\n"
+
+
+   SED="s/<processor>.*/\<processor\>$ID$PROCESSOR\<\/processor\>/"
+   sed -i $SED  "$PATH_SOURCE"/pom.xml
+   cd "$PATH_SOURCE"
+   mvn3 clean >/dev/null
+   mvn3 test >/dev/null
+
+echo -e "\n\n########################################################################################"
+echo "########################################################################################"
+echo " Copie du fichier rapport "
+echo "########################################################################################"
+echo -e "########################################################################################\n\n"
+   cp "$PATH_SOURCE"/target/surefire-reports/TEST-Devops.sources.AppTest.xml "$PATH_SOURCE"/target/surefire-reports/TEST-$PROCESSOR.xml
+done < processor.txt
+
+
