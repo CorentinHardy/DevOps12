a99335c63c3c8ece75047ba6003dc539e14cafbc
GregoryROBIN <robin.gregory.gr@gmail.com>
2016-03-08 14:05:32 +0100

    Application de mutation type binary avec + en - - en + / en *

--- ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
+++ ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
@@ -8,16 +8,25 @@ import spoon.reflect.declaration.CtElement;
 
 /** a trivial mutation operator that transforms all binary operators to minus ("-") */
 public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
-@Override
-public boolean isToBeProcessed(CtElement candidate) {
-return candidate instanceof CtBinaryOperator;
-}
-@Override
-public void process(CtElement candidate) {
-if (!(candidate instanceof CtBinaryOperator)) {
-return;
-}
-CtBinaryOperator op = (CtBinaryOperator)candidate;
-op.setKind(BinaryOperatorKind.MINUS);
-}
+	@Override
+	public boolean isToBeProcessed(CtElement candidate) {
+		return candidate instanceof CtBinaryOperator;
+	}
+	@Override
+	public void process(CtElement candidate) {
+		if (!(candidate instanceof CtBinaryOperator)) {
+		return;
+	}
+	CtBinaryOperator op = (CtBinaryOperator)candidate;
+	if(op.getKind().equals(BinaryOperatorKind.PLUS))// si c'est un + on met un -
+		op.setKind(BinaryOperatorKind.MINUS);
+	else if(op.getKind().equals(BinaryOperatorKind.MINUS))// si c'est un - on met un +
+		op.setKind(BinaryOperatorKind.PLUS);
+	else if(op.getKind().equals(BinaryOperatorKind.DIV))// si c'est un / on met un *
+		op.setKind(BinaryOperatorKind.MUL);
+	else if(op.getKind().equals(BinaryOperatorKind.MUL)){ // si c'est un * ou % on met un /
+		op.setKind(BinaryOperatorKind.DIV);
+	}
+
+	}
 }
