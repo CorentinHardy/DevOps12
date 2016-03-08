57d75ac361f7b89848e8cc1083a1a398dabc315e
GregoryROBIN <robin.gregory.gr@gmail.com>
2016-03-08 14:26:08 +0100

    application sur 20% des classes

--- ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
+++ ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
@@ -12,8 +12,43 @@ public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
 	public boolean isToBeProcessed(CtElement candidate) {
 		return candidate instanceof CtBinaryOperator;
 	}
+
 	@Override
-	public void process(CtElement candidate) {
+	//public void process(CtElement candidate) {
+		public process(CtElement element){
+			if(element instanceof CtClass){
+				CtClass maClass = (CtClass)candidate;
+				Random r = new Random();
+				//20% des classes
+				if(r > 0.2){
+					return;
+				}
+				List<CtElement> elementsToBeMutated = maClasse.getElements(new Filter<CtElement>(){
+					@Override
+					public boolean matches(CtElement arg0){
+						return this.isToBeProcessed(arg0);
+					}
+				});
+				for(CtElement e : elementsToBeMutated ){
+					if (!(candidate instanceof CtBinaryOperator)) {
+						return;
+					}
+					CtBinaryOperator op = (CtBinaryOperator)candidate;
+					if(op.getKind().equals(BinaryOperatorKind.PLUS))// si c'est un + on met un -
+						op.setKind(BinaryOperatorKind.MINUS);
+					else if(op.getKind().equals(BinaryOperatorKind.MINUS))// si c'est un - on met un +
+						op.setKind(BinaryOperatorKind.PLUS);
+					else if(op.getKind().equals(BinaryOperatorKind.DIV))// si c'est un / on met un *
+						op.setKind(BinaryOperatorKind.MUL);
+					else if(op.getKind().equals(BinaryOperatorKind.MUL)){ // si c'est un * ou % on met un /
+						op.setKind(BinaryOperatorKind.DIV);
+					}
+
+				}
+			}
+		}
+		
+		/*
 		if (!(candidate instanceof CtBinaryOperator)) {
 		return;
 	}
@@ -26,7 +61,7 @@ public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
 		op.setKind(BinaryOperatorKind.MUL);
 	else if(op.getKind().equals(BinaryOperatorKind.MUL)){ // si c'est un * ou % on met un /
 		op.setKind(BinaryOperatorKind.DIV);
-	}
+	}*/
 
-	}
+	
 }
