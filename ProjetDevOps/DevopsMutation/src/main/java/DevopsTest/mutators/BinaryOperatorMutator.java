b2727cbfa9def106742d7df78241af426d0ddc20
GregoryROBIN <robin.gregory.gr@gmail.com>
2016-03-09 23:04:33 +0100

    ok binary

--- ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
+++ ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
@@ -6,7 +6,7 @@ import spoon.reflect.declaration.CtElement;
 import spoon.support.reflect.code.CtBinaryOperatorImpl;
 import java.util.List;
 import java.util.Random;
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -22,30 +22,30 @@ public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
 	public void process(CtElement candidate) {
 		if (!(candidate instanceof CtBinaryOperatorImpl)) {
 		return;
-	}
+		}
 	
-	CtBinaryOperatorImpl op = (CtBinaryOperatorImpl) candidate;
-	
-	if (random.nextFloat()<MUTATION_PROBABILITY) {
+		CtBinaryOperatorImpl op = (CtBinaryOperatorImpl) candidate;
+		
+		if (random.nextFloat()<MUTATION_PROBABILITY) {
 			String Pos=op.getPosition().toString();
+			if(op.getKind().equals(BinaryOperatorKind.PLUS))
+				op.setKind(BinaryOperatorKind.MINUS);
+			else if(op.getKind().equals(BinaryOperatorKind.MINUS))
+				op.setKind(BinaryOperatorKind.PLUS);
+			else if(op.getKind().equals(BinaryOperatorKind.DIV))
+				op.setKind(BinaryOperatorKind.MUL);
+			else if(op.getKind().equals(BinaryOperatorKind.MUL))
+				op.setKind(BinaryOperatorKind.DIV);
 			try {
-				BufferedWriter modif = new BufferedWriter(new FileWriter("../tests_reports/modif.txt", true));
-				modif.write(Pos);
-				modif.newLine();
-				modif.flush();
+				PrintWriter modif = new PrintWriter(new FileWriter("../tests_reports/modif.txt", true));
+				modif.println(Pos);
 				modif.close();
-			} catch (IOException e) {
+			} 
+
+			catch (IOException e) {
 				e.printStackTrace();
 			}
-
-		if(op.getKind().equals(BinaryOperatorKind.PLUS))
-			op.setKind(BinaryOperatorKind.MINUS);
-		else if(op.getKind().equals(BinaryOperatorKind.MINUS))
-			op.setKind(BinaryOperatorKind.PLUS);
-		else if(op.getKind().equals(BinaryOperatorKind.DIV))
-			op.setKind(BinaryOperatorKind.MUL);
-		else if(op.getKind().equals(BinaryOperatorKind.MUL))
-			op.setKind(BinaryOperatorKind.DIV);
 		}
 	}
+	
 }
