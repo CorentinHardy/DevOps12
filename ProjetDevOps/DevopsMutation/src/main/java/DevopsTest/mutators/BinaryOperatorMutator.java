d3c6ce22bc1aabd16fc2565142e45e1c7d6accd8
GregoryROBIN <robin.gregory.gr@gmail.com>
2016-03-09 22:53:41 +0100

    log dans modif.txt des mutations : classe, ligne

--- ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
+++ ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/BinaryOperatorMutator.java
@@ -6,10 +6,13 @@ import spoon.reflect.declaration.CtElement;
 import spoon.support.reflect.code.CtBinaryOperatorImpl;
 import java.util.List;
 import java.util.Random;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
-	 private static final double MUTATION_PROBABILITY = 0.20;
+	private static final double MUTATION_PROBABILITY = 0.2;
 	private static final Random random = new Random();
 
 	@Override
@@ -24,7 +27,16 @@ public class BinaryOperatorMutator extends AbstractProcessor<CtElement> {
 	CtBinaryOperatorImpl op = (CtBinaryOperatorImpl) candidate;
 	
 	if (random.nextFloat()<MUTATION_PROBABILITY) {
-
+			String Pos=op.getPosition().toString();
+			try {
+				BufferedWriter modif = new BufferedWriter(new FileWriter("../tests_reports/modif.txt", true));
+				modif.write(Pos);
+				modif.newLine();
+				modif.flush();
+				modif.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 		if(op.getKind().equals(BinaryOperatorKind.PLUS))
 			op.setKind(BinaryOperatorKind.MINUS);
--- ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/UnaryOperatorMutator.java
+++ ProjetDevOps/DevopsMutation/src/main/java/DevopsTest/mutators/UnaryOperatorMutator.java
@@ -2,16 +2,20 @@ package DevopsTest.mutators;
 
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.*;
 import spoon.reflect.code.CtUnaryOperator;
 import spoon.reflect.code.UnaryOperatorKind;
 import spoon.reflect.declaration.CtElement;
 import java.util.List;
 import java.util.Random;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class UnaryOperatorMutator extends AbstractProcessor<CtElement> {
-	private static final double MUTATION_PROBABILITY = 0.20;
+	private static final double MUTATION_PROBABILITY = 0.2;
 	private static final Random random = new Random();
-	
 
 	@Override
 	public boolean isToBeProcessed(CtElement candidate){
@@ -25,13 +29,22 @@ public class UnaryOperatorMutator extends AbstractProcessor<CtElement> {
 		CtUnaryOperator op = (CtUnaryOperator) candidate;
 
 		if (random.nextFloat()<MUTATION_PROBABILITY) {
-
+			String Pos=op.getPosition().toString();			
 			if(op.getKind().equals(UnaryOperatorKind.POSTINC))
 				op.setKind(UnaryOperatorKind.POSTDEC);
 			if(op.getKind().equals(UnaryOperatorKind.PREINC))
 				op.setKind(UnaryOperatorKind.PREDEC);
 			if(op.getKind().equals(UnaryOperatorKind.NEG))
 				op.setKind(UnaryOperatorKind.POS);
+			try {
+				PrintWriter modif = new PrintWriter(new FileWriter("../tests_reports/modif.txt", true));
+				modif.println(Pos);
+					modif.close();
+			} 
+
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
